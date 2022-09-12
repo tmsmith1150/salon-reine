97949c6a8ed206636b60150f7b8109d3094f029e
Michelle Jordan <tmsmith1150@live.com>
2022-09-12 12:00:46 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import {BrowserRouter} from 'react-router-dom';
 import {StrictMode} from 'react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <StrictMode>
    <BrowserRouter basename={process.env.PUBLIC_URL}>
      <App />
    </BrowserRouter>
 </StrictMode>,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
